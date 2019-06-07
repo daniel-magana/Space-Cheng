cbafce2d5473939d9c8a6bd7def132422bcd3fd6
Daniel Magaña <51460863+daniel-magana@users.noreply.github.com>
2019-06-06 20:20:29 -0400

    Add files via upload

---
+++ BlackHoleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleScript : MonoBehaviour {
+
+    GameObject nave;
+    public float rango = 20f;
+    public float distancia;
+    public float potencia;
+
+	void Start () {
+        nave = GameObject.Find("Player");
+	}
+	
+	void Update ()
+    {
+        distancia = Vector2.Distance(transform.position, nave.transform.position);
+        if (distancia <= rango)
+        {
+            potencia = (rango-distancia)/2;
+        }
+        else
+        {
+            potencia = 0;
+        }
+        nave.transform.position = Vector3.MoveTowards(nave.transform.position, transform.position, Time.deltaTime * potencia);
+
+        transform.Rotate(0, 0, Time.deltaTime * 2);
+    }
+}
---
+++ FollowShip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowShip : MonoBehaviour {
+
+    public Transform player;
+
+	void Update () {
+        if (player != null)
+        {
+            Vector3 camara = new Vector3(player.position.x, player.position.y, 0);
+            transform.position = camara;
+        }
+	}
+}
